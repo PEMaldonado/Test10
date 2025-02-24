f5c78b8a5012171cac64eca741e26b5f68987d14
Paul-learn <pablomaldonado2087@gmail.com>
2025-02-24 11:26:55 -0300

    asd

--- index.php
+++ index.php
@@ -12,5 +12,47 @@ if ($hour < 12) {
     echo "Good afternoon!";
 } else {
     echo "Good evening!";
+}$hour = date('H');
+
+if ($hour < 12) {
+    echo "Good morning!";
+} elseif ($hour < 18) {
+    echo "Good afternoon!";
+} else {
+    echo "Good evening!";
+}$hour = date('H');
+
+if ($hour < 12) {
+    echo "Good morning!";
+} elseif ($hour < 18) {
+    echo "Good afternoon!";
+} else {
+    echo "Good evening!";
+}$hour = date('H');
+
+if ($hour < 12) {
+    echo "Good morning!";
+} elseif ($hour < 18) {
+    echo "Good afternoon!";
+} else {
+    echo "Good evening!";
+}$hour = date('H');
+
+if ($hour < 12) {
+    echo "Good morning!";
+} elseif ($hour < 18) {
+    echo "Good afternoon!";
+} else {
+    echo "Good evening!";
+}$hour = date('H');
+
+if ($hour < 12) {
+    echo "Good morning!";
+} elseif ($hour < 18) {
+    echo "Good afternoon!";
+} else {
+    echo "Good evening!";
 }
+
+
 ?>
