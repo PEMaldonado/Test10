f4e5a2ed069b74cf0083b55df38c5c1030045379
PEMaldonado <pablomaldonado2086@gmail.com>
2025-02-24 11:02:25 -0300

    first

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// index.php
+
+echo "Welcome to the index page!";
+
+// More complex logic
+$hour = date('H');
+
+if ($hour < 12) {
+    echo "Good morning!";
+} elseif ($hour < 18) {
+    echo "Good afternoon!";
+} else {
+    echo "Good evening!";
+}
+?>
